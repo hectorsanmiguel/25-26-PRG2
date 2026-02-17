328eb7f49a931129629d3ebf5c4641acef4fe69f
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-02-17 13:52:47 +0100

    Creado el bucle principal

--- entregas/sanmiguelHector/reto003/Buscaminas.java
+++ entregas/sanmiguelHector/reto003/Buscaminas.java
@@ -7,28 +7,33 @@ class Buscaminas {
     static final int NUMERO_DE_FILAS = 5;
     static final int NUMERO_DE_COLUMNAS = 7;
     static final int NUMERO_DE_MINAS_INICIALES = 5;
+    static final int NUMERO_DE_MINAS_ENCONTRADAS_PERMITIDAS = 3;
+    static final int NUMERO_DE_CASILLAS_SEGURAS = NUMERO_DE_FILAS * NUMERO_DE_COLUMNAS - NUMERO_DE_MINAS_INICIALES;
+
     static Scanner scanner = new Scanner(System.in);
 
     static char[][] tableroReal = new char[NUMERO_DE_FILAS][NUMERO_DE_COLUMNAS];
     static char[][] tableroVisible = new char[NUMERO_DE_FILAS][NUMERO_DE_COLUMNAS];
 
+    static int conteoMinas = 0;
+    static int conteoCasillasSeguras = 0;
+
     public static void main(String[] args) {
         crearTablero();
         colocarMinas();
         mostrarTablero();
-        while (!elJugadorHaGanado){
+
+        while (!elJuegoHaTerminado()) {
             turnoJugador();
             mostrarTablero();
         }
-
     }
 
     static void crearTablero() {
-
-        for (int filas = 0; filas < NUMERO_DE_FILAS; filas++) {
-            for (int columnas = 0; columnas < NUMERO_DE_COLUMNAS; columnas++) {
-                tableroReal[filas][columnas] = ' ';
-                tableroVisible[filas][columnas] = '-';
+        for (int f = 0; f < NUMERO_DE_FILAS; f++) {
+            for (int c = 0; c < NUMERO_DE_COLUMNAS; c++) {
+                tableroReal[f][c] = ' ';
+                tableroVisible[f][c] = '-';
             }
         }
     }
@@ -36,12 +41,12 @@ class Buscaminas {
     static void mostrarTablero() {
         System.out.print("   ");
         for (int columna = 0; columna < NUMERO_DE_COLUMNAS; columna++) {
-            System.out.print(columna + 1 + " ");
+            System.out.print((columna + 1) + " ");
         }
         System.out.println();
 
         for (int fila = 0; fila < NUMERO_DE_FILAS; fila++) {
-            System.out.print(fila + 1 + "  ");
+            System.out.print((fila + 1) + "  ");
             for (int columna = 0; columna < NUMERO_DE_COLUMNAS; columna++) {
                 System.out.print(tableroVisible[fila][columna] + " ");
             }
@@ -50,12 +55,10 @@ class Buscaminas {
     }
 
     static void colocarMinas() {
-
         Random random = new Random();
         int minasColocadas = 0;
 
         while (minasColocadas < NUMERO_DE_MINAS_INICIALES) {
-
             int fila = random.nextInt(NUMERO_DE_FILAS);
             int columna = random.nextInt(NUMERO_DE_COLUMNAS);
 
@@ -66,7 +69,7 @@ class Buscaminas {
         }
     }
 
-    static void turnoJugador(){
+    static void turnoJugador() {
         System.out.print("Introduce fila: ");
         int fila = scanner.nextInt();
 
@@ -76,25 +79,46 @@ class Buscaminas {
         validarEleccion(fila, columna);
     }
 
-    static void validarEleccion(int fila, int columna){
+    static void validarEleccion(int fila, int columna) {
 
         fila--;
         columna--;
 
         if (fila < 0 || fila >= NUMERO_DE_FILAS ||
-        columna < 0 || columna >= NUMERO_DE_COLUMNAS) {
+            columna < 0 || columna >= NUMERO_DE_COLUMNAS) {
 
             System.out.println("Coordenadas fuera de rango.");
             return;
         }
 
+        if (tableroVisible[fila][columna] != '-') {
+            System.out.println("Esa casilla ya fue descubierta.");
+            return;
+        }
+
         if (tableroReal[fila][columna] == '*') {
             tableroVisible[fila][columna] = '*';
+            conteoMinas++;
             System.out.println("Es una mina.");
         } else {
             tableroVisible[fila][columna] = '.';
+            conteoCasillasSeguras++;
             System.out.println("Casilla segura.");
         }
+    }
 
+    static boolean elJuegoHaTerminado() {
+
+        if (conteoMinas >= NUMERO_DE_MINAS_ENCONTRADAS_PERMITIDAS) {
+            System.out.println("Has explotado, perdiste.");
+            return true;
+        }
+
+        if (conteoCasillasSeguras == NUMERO_DE_CASILLAS_SEGURAS) {
+            System.out.println("Has sobrevivido, ganaste.");
+            return true;
+        }
+
+        return false;
     }
 }
