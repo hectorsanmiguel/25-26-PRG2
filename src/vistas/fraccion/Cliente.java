dcf86c15ab00329590520afe700e728ac03770c6
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-17 14:01:10 +0100

    Objeto Fraccion

---
+++ src/vistas/fraccion/Cliente.java
@@ -0,0 +1,11 @@
+package vistas.fraccion;
+
+public class Cliente {
+    public static void main(String[] args) {
+        
+        Fraccion dosCuartos = new Fraccion(2,4);
+        Fraccion tresQuintos = new Fraccion(3,5);
+
+
+    }
+}
