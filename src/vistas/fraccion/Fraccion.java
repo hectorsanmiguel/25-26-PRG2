6f73cb3cd680b77bbdcb3d0a26e65a33101b22d3
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-18 13:53:31 +0100

    Creados los constructores

--- src/vistas/fraccion/Fraccion.java
+++ src/vistas/fraccion/Fraccion.java
@@ -1,10 +1,32 @@
 package vistas.fraccion;
 
 public class Fraccion {
-    
-    public Fraccion(int numerador, int denominador){}
-    public Fraccion(int numero){}
-    public Fraccion(Fraccion fraccion){}
+    private int numerador;
+    private int denominador;
+
+
+
+
+
+    public Fraccion(int numerador, int denominador){
+        assert denominador != 0 : "Error. No puedes declarar el denominador como 0";
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+
+
+    public Fraccion(int numero){
+        this.numerador = numero;
+        this.denominador = 1;
+    }
+
+
+
+    public Fraccion(Fraccion fraccion){
+        this.numerador = fraccion.numerador;
+        this.denominador = fraccion.denominador;
+    }
 
     public Fraccion clonar(){}
 
