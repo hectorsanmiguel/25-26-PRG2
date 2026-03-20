141c45a33dbe1f5c0bbddb527155010058a40b7b
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-20 10:44:31 +0100

    Documentos con buen formato

--- entregas/sanmiguelHector/reto004/Cliente.java
+++ entregas/sanmiguelHector/reto004/Cliente.java
@@ -2,19 +2,17 @@ package vistas.fraccion;
 
 public class Cliente {
     public static void main(String[] args) {
-        
-        Fraccion dosCuartos = new Fraccion(2,4);
-        Fraccion tresQuintos = new Fraccion(3,5);
+
+        Fraccion dosCuartos = new Fraccion(2, 4);
+        Fraccion tresQuintos = new Fraccion(3, 5);
         Fraccion cinco = new Fraccion(5);
         Fraccion ceroMedios = new Fraccion(0, 2);
 
         Fraccion cuatrocientosDecimos = new Fraccion(400, 10);
 
-
         System.out.println("Antes de simplificar: " + cuatrocientosDecimos);
         cuatrocientosDecimos.simplificar();
         System.out.println("Después de simplificar: " + cuatrocientosDecimos);
-
 
         System.out.println("¿Es mayor 2/4 que 3/5? " + dosCuartos.esMayorQueFraccion(tresQuintos));
         System.out.println("¿Es mayor 5 que 3/5? " + cinco.esMayorQueFraccion(tresQuintos));
--- entregas/sanmiguelHector/reto004/Fraccion.java
+++ entregas/sanmiguelHector/reto004/Fraccion.java
@@ -4,45 +4,38 @@ public class Fraccion {
     private int numerador;
     private int denominador;
 
-
-
-
-
-    public Fraccion(int numerador, int denominador){
+    public Fraccion(int numerador, int denominador) {
         assert denominador != 0 : "Error. No puedes declarar el denominador como 0";
         this.numerador = numerador;
         this.denominador = denominador;
     }
 
-
-
-    public Fraccion(int numero){
+    public Fraccion(int numero) {
         this.numerador = numero;
         this.denominador = 1;
     }
 
-
-
-    public Fraccion(Fraccion fraccion){
+    public Fraccion(Fraccion fraccion) {
         this.numerador = fraccion.numerador;
         this.denominador = fraccion.denominador;
     }
 
-    public Fraccion clonar(){
+    public Fraccion clonar() {
         return new Fraccion(this);
     }
 
-    public Fraccion sumar(Fraccion fraccion){
+    public Fraccion sumar(Fraccion fraccion) {
         assert fraccion != null : "La fracción no puede ser nula";
 
-        int nuevoNumerador = (this.numerador * fraccion.denominador)+(this.denominador * fraccion.numerador);
+        int nuevoNumerador = (this.numerador * fraccion.denominador) + (this.denominador * fraccion.numerador);
         int nuevoDenominador = this.denominador * fraccion.denominador;
 
         this.numerador = nuevoNumerador;
         this.denominador = nuevoDenominador;
         return this;
     }
-    public Fraccion restar(Fraccion fraccion){
+
+    public Fraccion restar(Fraccion fraccion) {
         assert fraccion != null : "La fracción no puede ser nula";
 
         fraccion.oponer();
@@ -53,7 +46,8 @@ public class Fraccion {
 
         return this;
     }
-    public Fraccion multiplicar(Fraccion fraccion){
+
+    public Fraccion multiplicar(Fraccion fraccion) {
         assert fraccion != null : "La fracción no puede ser nula";
 
         int nuevoNumerador = this.numerador * fraccion.numerador;
@@ -64,9 +58,10 @@ public class Fraccion {
 
         return this;
     }
-    public Fraccion dividir(Fraccion fraccion){
+
+    public Fraccion dividir(Fraccion fraccion) {
         assert fraccion != null : "La fracción no puede ser nula";
-        assert fraccion.numerador != 0: "El numerador no puede ser cero";
+        assert fraccion.numerador != 0 : "El numerador no puede ser cero";
 
         fraccion.invertir();
 
@@ -77,15 +72,16 @@ public class Fraccion {
         return this;
     }
 
-    private int compararFracciones(Fraccion fraccion){
+    private int compararFracciones(Fraccion fraccion) {
         return (this.numerador * fraccion.denominador) - (this.denominador * fraccion.numerador);
     }
 
-    public boolean esMayorQueFraccion(Fraccion fraccion){
-        return compararFracciones(fraccion) > 0; 
+    public boolean esMayorQueFraccion(Fraccion fraccion) {
+        return compararFracciones(fraccion) > 0;
     }
-    public boolean esMenorQueFraccion(Fraccion fraccion){
-        return compararFracciones(fraccion) < 0; 
+
+    public boolean esMenorQueFraccion(Fraccion fraccion) {
+        return compararFracciones(fraccion) < 0;
     }
 
     public boolean esMayorQueNumero(int numero) {
@@ -96,20 +92,20 @@ public class Fraccion {
         return esMenorQueFraccion(new Fraccion(numero));
     }
 
-    public boolean esIgualFraccion(Fraccion fraccion){
+    public boolean esIgualFraccion(Fraccion fraccion) {
         return compararFracciones(fraccion) == 0;
     }
 
-    public boolean esIgualNumero(int numero){
+    public boolean esIgualNumero(int numero) {
         return esIgualFraccion(new Fraccion(numero));
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return this.numerador + "/" + this.denominador;
     }
 
-    public Fraccion invertir(){
+    public Fraccion invertir() {
         assert this.numerador != 0 : "No se puede invertir una fracción con numerador 0";
 
         int auxiliar = this.numerador;
@@ -119,7 +115,7 @@ public class Fraccion {
         return this;
     }
 
-    public Fraccion oponer(){
+    public Fraccion oponer() {
         this.numerador *= -1;
 
         return this;
@@ -134,7 +130,7 @@ public class Fraccion {
         return numerador;
     }
 
-    public Fraccion simplificar(){
+    public Fraccion simplificar() {
         int maximoComunDivisor = calcularMCD(numerador, denominador);
 
         this.numerador /= maximoComunDivisor;
